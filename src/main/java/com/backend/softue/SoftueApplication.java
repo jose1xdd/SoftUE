6259ea27e910026d0c7762e34b9086528aeed3ba
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-05-10 22:12:27 -0500

    initial project staff

---
+++ src/main/java/com/backend/softue/SoftueApplication.java
@@ -0,0 +1,13 @@
+package com.backend.softue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/softue/SoftueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.softue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
