d85e88d1c0a5322b13bc6adf1c9dc84f22fd6cb6
Julian <josejulianalre@ufps.edu.co>
2023-05-21 13:38:03 -0500

    Se creo el registrar docente

--- src/main/java/com/backend/softue/services/DocenteServices.java
+++ src/main/java/com/backend/softue/services/DocenteServices.java
@@ -1,4 +1,22 @@
 package com.backend.softue.services;
 
+import com.backend.softue.models.Docente;
+import com.backend.softue.models.Estudiante;
+import com.backend.softue.models.User;
+import com.backend.softue.repositories.DocenteRepository;
+import com.backend.softue.repositories.EstudianteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DocenteServices {
+    @Autowired
+    private DocenteRepository docenteRepository;
+
+    @Autowired
+    private UserServices usuarioServices;
+    public void registrarDocente(Docente docente) {
+        usuarioServices.registerUser((User) docente);
+        docenteRepository.save(docente);
+    }
 }
