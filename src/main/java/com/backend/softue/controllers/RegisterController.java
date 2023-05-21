780c4c944162c4723517cc9e71de5f6684d4d3ff
CarlosSalazar0408 <61851760+CarlosSalazar0408@users.noreply.github.com>
2023-05-21 13:14:28 -0500

    Se creo el registrar estudiante (#21)
    * Refactor name bug
    
    * Creación tabla entidad financiadora y foto_entidad_financiadora
    
    * Creación de los modelos de idea y plan de negocio
    
    * Se crearon los modelos de la evalucación de idea y plan de negocio
    
    * Modelo de formato
    
    * Se agregan tutores a las ideas y planes
    
    * Creado observacion plan
    
    * Se agregaron los modelos de la tabla intermedia de la calificacion en una evaluación por parte del docente
    
    * Se terminaron las calificaciones para los planes de negocio
    
    * Refinamiento de modelos
    
    * Creacion de registrar estudiante
    
    ---------
    
    Co-authored-by: Julian <josejulianalre@ufps.edu.co>

--- src/main/java/com/backend/softue/controllers/RegisterController.java
+++ src/main/java/com/backend/softue/controllers/RegisterController.java
@@ -1,8 +1,11 @@
 package com.backend.softue.controllers;
 
+import com.backend.softue.models.Estudiante;
 import com.backend.softue.models.User;
+import com.backend.softue.services.EstudianteServices;
 import com.backend.softue.services.UserServices;
 import com.backend.softue.utils.response.ErrorFactory;
+import com.backend.softue.utils.response.ResponseConfirmation;
 import com.backend.softue.utils.response.ResponseError;
 import com.backend.softue.utils.response.ResponseToken;
 import jakarta.validation.Valid;
@@ -21,8 +24,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegisterController {
     @Autowired
     UserServices userServices;
+
+    @Autowired
+    EstudianteServices estudianteServices;
+
     @Autowired
     ErrorFactory errorFactory;
+
     @PostMapping()
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult bindingResult) {
        try {
@@ -35,5 +43,20 @@ public class RegisterController {
        catch (Exception e ){
             return new ResponseEntity<ResponseError>(new ResponseError(e.getMessage()), HttpStatus.BAD_REQUEST);
        }
+    }
+
+    @PostMapping("/estudiante")
+    public ResponseEntity<?> registrarEstudiante(@Valid @RequestBody Estudiante estudiante, BindingResult bindingResult) {
+        try {
+            if (bindingResult.hasErrors()) {
+                String errorMessages = errorFactory.errorGenerator(bindingResult);
+                return new ResponseEntity<ResponseError>(new ResponseError(errorMessages), HttpStatus.BAD_REQUEST);
+            }
+            this.estudianteServices.registrarEstudiante(estudiante);
+            return new ResponseEntity<ResponseConfirmation>(new ResponseConfirmation("El estudiante se registro correctamente"), HttpStatus.OK);
+        }
+        catch (Exception e ){
+            return new ResponseEntity<ResponseError>(new ResponseError(e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
     }
 }
