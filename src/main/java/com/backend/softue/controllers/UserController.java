e5ce912889311a41614da93f54c2468d81062730
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-05-21 22:03:25 -0500

    merge solved

--- src/main/java/com/backend/softue/controllers/UserController.java
+++ src/main/java/com/backend/softue/controllers/UserController.java
@@ -1,30 +1,17 @@
 package com.backend.softue.controllers;
 
-<<<<<<< HEAD
 import com.backend.softue.security.Hashing;
 import com.backend.softue.services.UserServices;
 import com.backend.softue.utils.checkSession.CheckSession;
 import com.backend.softue.utils.response.*;
-=======
-import com.backend.softue.models.User;
-import com.backend.softue.services.UserServices;
-import com.backend.softue.utils.checkSession.CheckSession;
-import com.backend.softue.utils.response.ErrorFactory;
-import com.backend.softue.utils.response.ResponseConfirmation;
-import com.backend.softue.utils.response.ResponseError;
-import jakarta.validation.Valid;
->>>>>>> 9e2867c5c806a2eb90072d620cf38a6a574a66cd
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
-@RequestMapping(value = {"/coordinador", "/administrativo"})
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserServices userServices;
@@ -54,77 +41,28 @@ public class UserController {
         }
     }
 
-<<<<<<< HEAD
     @GetMapping("/forgotPassword/{email}")
     public ResponseEntity<?> forgotPassword(@PathVariable("email") String email) {
         try {
             return ResponseEntity.ok(new ResponseToken(this.userServices.forgotPassword(email)));
         } catch (Exception e) {
-=======
-    @CheckSession(permitedRol ={"coordinador", "administrativo"})
-    @PatchMapping("/update")
-    public ResponseEntity<?> actualizar(@RequestHeader("X-Softue-JWT") String jwt, @Valid @RequestBody User user, BindingResult bindingResult) {
-        try {
-            if (bindingResult.hasErrors()) {
-                String errorMessages = errorFactory.errorGenerator(bindingResult);
-                return new ResponseEntity<ResponseError>(new ResponseError(errorMessages), HttpStatus.BAD_REQUEST);
-            }
-            this.userServices.actualizarUsuario(user, jwt);
-            return ResponseEntity.ok(new ResponseConfirmation("El usuario ha sido actualizado correctamente"));
-        }
-        catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ResponseError(e.getMessage()));
-        }
-    }
-    @CheckSession(permitedRol ={"estudiante", "coordinador", "administrativo", "docente"})
-    @GetMapping("/{email}")
-    public ResponseEntity<?> visualizar(@RequestHeader("X-Softue-JWT") String jwt, @PathVariable String email) {
-        try {
-            return ResponseEntity.ok(this.userServices.obtenerUsuario(email));
-        }
-        catch (Exception e) {
->>>>>>> 9e2867c5c806a2eb90072d620cf38a6a574a66cd
             return ResponseEntity.badRequest().body(new ResponseError(e.getMessage()));
         }
     }
 
-<<<<<<< HEAD
     @PatchMapping("/resetPassword")
     public ResponseEntity resetPassword(@RequestHeader("X-Softue-Reset") String token, @RequestBody RequestPassword password) {
         try {
             this.userServices.resetPassword(token, password.getPassword());
             return ResponseEntity.ok(new ResponseConfirmation("Contraseña Restablecida"));
         } catch (Exception e) {
-=======
-    @CheckSession(permitedRol ={"estudiante", "coordinador", "administrativo", "docente"})
-    @GetMapping(value = "/foto/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<?> obtenerFoto(@RequestHeader("X-Softue-JWT") String jwt, @PathVariable String id) {
-        try {
-            return ResponseEntity.ok(this.userServices.obtenerFoto(id));
-        }
-        catch (Exception e){
->>>>>>> 9e2867c5c806a2eb90072d620cf38a6a574a66cd
             return ResponseEntity.badRequest().body(new ResponseError(e.getMessage()));
         }
     }
 
-<<<<<<< HEAD
     @GetMapping("/test")
     public Boolean test(@RequestHeader("password")String password,@RequestHeader("nuevapass") String nuevaPasss){
         return this.encryp.validate(nuevaPasss,password);
     }
 
-=======
-    @CheckSession(permitedRol ={"coordinador", "administrativo"})
-    @GetMapping("/deshabilitarUsuario/{email}")
-    public ResponseEntity<?> deshabilitarUsuario(@RequestHeader("X-Softue-JWT") String jwt, @PathVariable String email) {
-        try {
-            this.userServices.deshabilitarUsuario(email);
-            return ResponseEntity.ok(new ResponseConfirmation("El usuario ha sido deshabilitado correctamente"));
-        }
-        catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ResponseError(e.getMessage()));
-        }
-    }
->>>>>>> 9e2867c5c806a2eb90072d620cf38a6a574a66cd
 }
