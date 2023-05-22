7f632068df9dbbff2a5e3a05410303e2a40f3c20
AlexaVJ <67123457+AlexaVJ@users.noreply.github.com>
2023-05-22 16:02:32 -0500

    Funcionalidad crear idea de negocio (#46)
    * Refactor name bug
    
    * Creación entidad estudiante y docente
    
    * Creación tabla entidad financiadora y foto_entidad_financiadora
    
    * Creación de los modelos de idea y plan de negocio
    
    * Se crearon los modelos de la evalucación de idea y plan de negocio
    
    * Merge modificaciones de idea de negocio
    
    * Creación de las entidades docente de apoyo de idea de negocio y de plan de negocio
    
    * Modelo de formato
    
    * Se agregan tutores a las ideas y planes
    
    * CActualización campos de DocenteIdeaKey y DocentePlanKey
    
    * Creado observacion plan
    
    * Se agregaron los modelos de la tabla intermedia de la calificacion en una evaluación por parte del docente
    
    * Se terminaron las calificaciones para los planes de negocio
    
    * se termino el modelo de observacionIdea
    
    * Creacion de las relaciones entre entidad estudiante y entidad idea de negocio y plan de negocio, junto con actualizaciones a los nombres de las tablas
    
    * Creacion documentos de idea de negocio y plan de negocio
    
    * Refinamiento de modelos
    
    * Mensajes de error
    
    * ActualizaciÃón de los msajes de error
    
    * Se terminaron los mensajes de error
    
    * Creacion de registrar estudiante
    
    * Se creo el registrar docente
    
    * Arreglado conflicto con los imports
    
    * Creación de bean de roles y método de actualizar usuario
    
    * Error de comas en el bean de roles
    
    * Se agrego la actualización de estudiantes
    
    * Funcionalidad de la creación de entidades financiadoras
    
    * Se creo la funcionalidad de actualizar docente
    
    * Se termino el obtener información y foto de perfil de un usuario
    
    * Funcionalidad de lactualización y eliminación de entidades financiadoras
    
    * Se arreglaron bugs menores
    
    * Se completo la visualizacion de un docente
    
    * Se implemento todo lo referente a deshabilitar usuarios y estudiantes
    
    * Finalizacion de la funcionalidad de deshabilitar docentes
    
    * Todo bien cachaco
    
    * Funcionalidad crear, actualizar, guardar foto, eliminar y listar de entidades financiadoras
    
    * Funcionalidad listar entidades financiadoras con foto
    
    * CFuncionalidad crear una idea de negocio
    
    ---------
    
    Co-authored-by: Carlos Salazar <carlosalbertosame@ufps.edu.co>
    Co-authored-by: Julian <josejulianalre@ufps.edu.co>

---
+++ src/main/java/com/backend/softue/services/IdeaPlanteadaServices.java
@@ -0,0 +1,64 @@
+package com.backend.softue.services;
+
+import com.backend.softue.models.Estudiante;
+import com.backend.softue.models.EstudianteIdeaKey;
+import com.backend.softue.models.IdeaNegocio;
+import com.backend.softue.models.IdeaPlanteada;
+import com.backend.softue.repositories.EstudianteRepository;
+import com.backend.softue.repositories.IdeaNegocioRepository;
+import com.backend.softue.repositories.IdeaPlanteadaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class IdeaPlanteadaServices {
+
+    @Autowired
+    private IdeaNegocioRepository ideaNegocioRepository;
+
+    @Autowired
+    private IdeaPlanteadaRepository ideaPlanteadaRepository;
+
+    @Autowired
+    private EstudianteRepository estudianteRepository;
+
+    public void crear(IdeaNegocio ideaNegocio) {
+        int numeroEstudiantes = 0;
+        Estudiante estudiante = null;
+        EstudianteIdeaKey id = null;
+        IdeaPlanteada ideaPlanteada = null;
+        String correo = "";
+
+        IdeaNegocio resultado = this.ideaNegocioRepository.findByTitulo(ideaNegocio.getTitulo());
+        if(resultado == null) throw new RuntimeException("No existe la idea de negocio a la cuál se le desea agregar integrantes estudiantes");
+
+        integrantesExistentes(ideaNegocio);
+
+        numeroEstudiantes = ideaNegocio.getCorreoEstudiantesIntegrantes().length;
+        for(int i = 0; i < numeroEstudiantes; i++) {
+            correo = ideaNegocio.getCorreoEstudiantesIntegrantes()[i];
+            estudiante = this.estudianteRepository.findByCorreo(correo);
+            id = new EstudianteIdeaKey(estudiante.getCodigo(), ideaNegocio.getId());
+            ideaPlanteada = new IdeaPlanteada(id, estudiante, resultado);
+            this.ideaPlanteadaRepository.save(ideaPlanteada);
+        }
+
+    }
+
+    public void integrantesExistentes(IdeaNegocio ideaNegocio){
+        int numeroEstudiantes = 0;
+        String correo = "";
+        Estudiante estudiante = null;
+
+        numeroEstudiantes = ideaNegocio.getCorreoEstudiantesIntegrantes().length;
+        for(int i = 0; i < numeroEstudiantes; i++) {
+            correo = ideaNegocio.getCorreoEstudiantesIntegrantes()[i];
+            estudiante = this.estudianteRepository.findByCorreo(correo);
+            if (estudiante == null)
+                throw new RuntimeException("El estudiante integrante que tiene como correo " + correo + " no existe");
+        }
+    }
+}
