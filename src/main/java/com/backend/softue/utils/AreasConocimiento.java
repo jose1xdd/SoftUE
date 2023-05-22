d800950ff8d9c0c75e820d8cbea8aed3e64a6aa2
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-05-22 13:32:45 -0500

    adding rol restriccion in listarUsuarioRol

---
+++ src/main/java/com/backend/softue/utils/AreasConocimiento.java
@@ -0,0 +1,8 @@
+package com.backend.softue.utils;
+
+import java.util.Set;
+
+public class AreasConocimiento {
+    private final Set<String> areasConocimiento = Set.of("Actividad minera", "Actividad agropecuaria", "Actividad comercial",
+            "Actividad de Servicios", "Actividad Industrial");
+}
