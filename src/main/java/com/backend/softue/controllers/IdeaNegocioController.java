315c2b4235c94ec4c513adfb97f12207355d5af7
AlexaVJ <67123457+AlexaVJ@users.noreply.github.com>
2023-05-22 20:37:52 -0500

    Creación de la funcionalidad gestionar integrantes de una idea de negocio (#52)
    * Refactor name bug
    
    * Creación entidad estudiante y docente
    
    * Creación tabla entidad financiadora y foto_entidad_financiadora
    
    * Creación de los modelos de idea y plan de negocio
    
    * Se crearon los modelos de la evalucación de idea y plan de negocio
    
    * Merge modificaciones de idea de negocio
    
    * Creación de las entidades docente de apoyo de idea de negocio y de plan de negocio
    
    * Modelo de formato
    
    * Se agregan tutores a las ideas y planes
    
    * CActualización campos de DocenteIdeaKey y DocentePlanKey
    
    * Creado observacion plan
    
    * Se agregaron los modelos de la tabla intermedia de la calificacion en una evaluación por parte del docente
    
    * Se terminaron las calificaciones para los planes de negocio
    
    * se termino el modelo de observacionIdea
    
    * Creacion de las relaciones entre entidad estudiante y entidad idea de negocio y plan de negocio, junto con actualizaciones a los nombres de las tablas
    
    * Creacion documentos de idea de negocio y plan de negocio
    
    * Refinamiento de modelos
    
    * Mensajes de error
    
    * ActualizaciÃón de los msajes de error
    
    * Se terminaron los mensajes de error
    
    * Creacion de registrar estudiante
    
    * Se creo el registrar docente
    
    * Arreglado conflicto con los imports
    
    * Creación de bean de roles y método de actualizar usuario
    
    * Error de comas en el bean de roles
    
    * Se agrego la actualización de estudiantes
    
    * Funcionalidad de la creación de entidades financiadoras
    
    * Se creo la funcionalidad de actualizar docente
    
    * Se termino el obtener información y foto de perfil de un usuario
    
    * Funcionalidad de lactualización y eliminación de entidades financiadoras
    
    * Se arreglaron bugs menores
    
    * Se completo la visualizacion de un docente
    
    * Se implemento todo lo referente a deshabilitar usuarios y estudiantes
    
    * Finalizacion de la funcionalidad de deshabilitar docentes
    
    * Todo bien cachaco
    
    * Funcionalidad crear, actualizar, guardar foto, eliminar y listar de entidades financiadoras
    
    * Funcionalidad listar entidades financiadoras con foto
    
    * CFuncionalidad crear una idea de negocio
    
    * Funcionalidad actualizar titulo de una idea de negocio
    
    * Creación de la funcionalidad gestionar integrantes de una idea de negocio
    
    ---------
    
    Co-authored-by: Carlos Salazar <carlosalbertosame@ufps.edu.co>
    Co-authored-by: Julian <josejulianalre@ufps.edu.co>

--- src/main/java/com/backend/softue/controllers/IdeaNegocioController.java
+++ src/main/java/com/backend/softue/controllers/IdeaNegocioController.java
@@ -1,6 +1,7 @@
 package com.backend.softue.controllers;
 
 
+import com.backend.softue.models.Estudiante;
 import com.backend.softue.models.IdeaNegocio;
 import com.backend.softue.services.IdeaNegocioServices;
 import com.backend.softue.services.IdeaPlanteadaServices;
@@ -45,4 +46,54 @@ public class IdeaNegocioController {
         }
     }
 
+    @CheckSession(permitedRol ={"estudiante"})
+    @PatchMapping("/titulo")
+    public ResponseEntity<?> actualizarTitulo(@RequestHeader("X-Softue-JWT") String jwt,
+                                              @Valid @RequestBody IdeaNegocio ideaNegocio,
+                                              BindingResult bindingResult) {
+        try {
+            if (bindingResult.hasErrors()) {
+                String errorMessages = errorFactory.errorGenerator(bindingResult);
+                return ResponseEntity.badRequest().body(new ResponseError("Input Error",errorMessages,"Bad Request"));
+            }
+            this.ideaNegocioServices.actualizarTitulo(ideaNegocio, jwt);
+            return ResponseEntity.ok(new ResponseConfirmation("Título de la idea de negocio actualizado correctamente"));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ResponseError(e.getClass().toString(),e.getMessage(),e.getStackTrace()[0].toString()));
+        }
+    }
+
+    @CheckSession(permitedRol ={"docente"})
+    @PatchMapping("/agregarIntegrante/{titulo}")
+    public ResponseEntity<?> agregarIntegrante(@RequestHeader("X-Softue-JWT") String jwt,
+                                               @Valid @RequestBody Estudiante estudiante,
+                                               BindingResult bindingResult, @PathVariable String titulo) {
+        try {
+            if (bindingResult.hasErrors()) {
+                String errorMessages = errorFactory.errorGenerator(bindingResult);
+                return ResponseEntity.badRequest().body(new ResponseError("Input Error",errorMessages,"Bad Request"));
+            }
+            this.ideaNegocioServices.agregarIntegrante(estudiante, titulo, jwt);
+            return ResponseEntity.ok(new ResponseConfirmation("Estudiante agregado a la idea de negocio correctamente"));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ResponseError(e.getClass().toString(),e.getMessage(),e.getStackTrace()[0].toString()));
+        }
+    }
+
+    @CheckSession(permitedRol ={"docente"})
+    @DeleteMapping("/eliminarIntegrante/{titulo}")
+    public ResponseEntity<?> eliminarIntegrante(@RequestHeader("X-Softue-JWT") String jwt,
+                                               @Valid @RequestBody Estudiante estudiante,
+                                               BindingResult bindingResult, @PathVariable String titulo) {
+        try {
+            if (bindingResult.hasErrors()) {
+                String errorMessages = errorFactory.errorGenerator(bindingResult);
+                return ResponseEntity.badRequest().body(new ResponseError("Input Error",errorMessages,"Bad Request"));
+            }
+            this.ideaNegocioServices.eliminarIntegrante(estudiante, titulo, jwt);
+            return ResponseEntity.ok(new ResponseConfirmation("Estudiante eliminado de la idea de negocio correctamente"));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ResponseError(e.getClass().toString(),e.getMessage(),e.getStackTrace()[0].toString()));
+        }
+    }
 }
