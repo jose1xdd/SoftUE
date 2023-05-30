2ba89412cc315d2838a2bae42d3c63cabf7a9100
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-05-29 20:17:00 -0500

    error message in password filter added

--- src/main/java/com/backend/softue/models/User.java
+++ src/main/java/com/backend/softue/models/User.java
@@ -59,7 +59,7 @@ public class User {
     @NotBlank(message = "Error: El campo 'contrasenia' no puede estar en blanco. Por favor, asegurese de proporcionar un valor valido para la contrasenia del usuario.")
     @NotNull(message = "Error: El campo 'contrasenia' no puede ser nulo. Por favor, asegurese de proporcionar un valor valido para la contrasenia del usuario.")
     @Column(nullable = false)
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E]).{6,}$")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E]).{6,}$",message = "Error: La contraseña que ha ingresado debe cumplir con los siguientes requisitos minimos : almenos 6 caracteres ,una letra en mayuscula, un numero y un caracter especial")
     private String contrasenia;
 
     @NotBlank(message = "Error: El campo 'tipo_usuario' no puede estar en blanco. Por favor, asegurese de proporcionar un valor valido para el tipo del usuario.")
