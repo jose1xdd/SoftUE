ecfbe792492f46fab91f001a5e1d8ed1ebfd4cf5
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-05-31 05:02:08 -0500

    finish

--- src/main/java/com/backend/softue/controllers/TestController.java
+++ src/main/java/com/backend/softue/controllers/TestController.java
@@ -5,9 +5,11 @@ import com.backend.softue.utils.response.ResponseError;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/test")
 public class TestController {
     @GetMapping("/")
     public ResponseEntity<?> logout(@RequestHeader("X-Softue-JWT") String jwt) {
