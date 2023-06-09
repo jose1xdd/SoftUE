d49543db31ee3350338d8da7b80254d8c38e7c63
Carlos Salazar <carlosalbertosame@ufps.edu.co>
2023-06-08 22:38:46 -0500

    Se creo lo referente a preguntas

--- src/main/java/com/backend/softue/services/RespuestaServices.java
+++ src/main/java/com/backend/softue/services/RespuestaServices.java
@@ -3,16 +3,20 @@ package com.backend.softue.services;
 import com.backend.softue.models.Pregunta;
 import com.backend.softue.models.Respuesta;
 import com.backend.softue.repositories.RespuestaRepository;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Setter
 @Service
 public class RespuestaServices {
 
     @Autowired
     private RespuestaRepository respuestaRepository;
 
-    @Autowired
+
     private PreguntaServices preguntaServices;
 
     public void crear(String contenido, Integer valor, Integer preguntaId) {
@@ -22,5 +26,49 @@ public class RespuestaServices {
             throw new RuntimeException("El valor de la respuesta no puede ser nulo");
         Pregunta pregunta = this.preguntaServices.obtener(preguntaId);
         this.respuestaRepository.save(new Respuesta(null, contenido, valor, pregunta));
+    }
+
+    public void eliminar(Integer id) {
+        if (id == null)
+            throw new RuntimeException("No se tiene un id con el que eliminar la respuesta");
+        if (!this.respuestaRepository.existsById(id))
+            throw new RuntimeException("La respuesta a eliminar no existe");
+        this.respuestaRepository.deleteById(id);
+    }
+
+    public void actualizar(Integer id, String contenido, Integer valor, Integer preguntaId) {
+        if (id == null)
+            throw new RuntimeException("El id con el que se desea buscar la respuesta no existe");
+        if (contenido.equals(""))
+            throw new RuntimeException("El contenido no puede estar vacío");
+        Respuesta respuesta = this.respuestaRepository.findById(id).get();
+        if (respuesta == null)
+            throw new RuntimeException("La respuesta con ese id no existe");
+        if (contenido != null)
+            respuesta.setContenido(contenido);
+        if (valor != null)
+            respuesta.setValor(valor);
+        if (preguntaId != null) {
+            Pregunta pregunta = this.preguntaServices.obtener(preguntaId);
+            if (pregunta == null)
+                throw new RuntimeException("No existe una pregunta con ese nombre");
+            respuesta.setPreguntaId(pregunta);
+        }
+        this.respuestaRepository.save(respuesta);
+    }
+
+    public List<Respuesta> obtenerRespuestas(Integer id) {
+        if (id == null)
+            throw new RuntimeException("No se pueden obtener las respuestas de un id null");
+        Pregunta pregunta = this.preguntaServices.obtener(id);
+        if (pregunta == null)
+            throw new RuntimeException("La pregunta de las que se desea obtener las respuestas no existe");
+        return this.respuestaRepository.findByPreguntaId(pregunta);
+    }
+
+    public List<Respuesta> obtenerRespuestas(Pregunta pregunta) {
+        if (pregunta == null)
+            throw new RuntimeException("No se pueden obtener las respuestas de un null");
+        return this.respuestaRepository.findByPreguntaId(pregunta);
     }
 }
