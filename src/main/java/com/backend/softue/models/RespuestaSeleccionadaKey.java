686eaac0ab7597483ac063923aa75f4f83303279
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-06-11 16:59:41 -0500

    fix

--- src/main/java/com/backend/softue/models/RespuestaSeleccionadaKey.java
+++ src/main/java/com/backend/softue/models/RespuestaSeleccionadaKey.java
@@ -3,16 +3,14 @@ package com.backend.softue.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class RespuestaSeleccionadaKey {
 
     @NotNull()
