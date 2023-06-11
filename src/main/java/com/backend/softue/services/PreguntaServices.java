faaf216f265082685503e713fdef6c4d348fe1fe
Carlos Salazar <carlosalbertosame@ufps.edu.co>
2023-06-11 15:44:24 -0500

    Se agrego la propiedad para las preguntas eliminadas

--- src/main/java/com/backend/softue/services/PreguntaServices.java
+++ src/main/java/com/backend/softue/services/PreguntaServices.java
@@ -33,11 +33,11 @@ public class PreguntaServices {
         if (enunciado == null || enunciado.equals(""))
             throw new RuntimeException("El enunciado no puede estar vacío");
         ComponenteCompetencias componenteCompetencias = this.componenteCompetenciasServices.obtener(nombreComponente);
-        this.preguntaRepository.save(new Pregunta(null, enunciado, componenteCompetencias, null, null));
+        this.preguntaRepository.save(new Pregunta(null, enunciado, false, componenteCompetencias, null, null));
     }
 
     public List<Pregunta> listar() {
-        List<Pregunta> resultado = this.preguntaRepository.findAll();
+        List<Pregunta> resultado = this.preguntaRepository.findByEliminada(false);
         for (Pregunta pregunta : resultado) {
             pregunta.setListaRespuestas(this.respuestaServices.obtenerRespuestas(pregunta.getId()));
         }
@@ -50,7 +50,7 @@ public class PreguntaServices {
         if (enunciado.equals(""))
             throw new RuntimeException("El enunciado no puede estar vacío");
         Pregunta pregunta = this.preguntaRepository.findById(Integer.parseInt(id)).get();
-        if (pregunta == null)
+        if (pregunta == null || pregunta.getEliminada())
             throw new RuntimeException("La pregunta con ese id no existe");
         if (enunciado != null)
             pregunta.setEnunciado(enunciado);
@@ -66,16 +66,18 @@ public class PreguntaServices {
     public void eliminar(Integer id) {
         if (id == null)
             throw new RuntimeException("No se tiene un id con el que eliminar la pregunta");
-        if (!this.preguntaRepository.existsById(id))
+        Pregunta pregunta = this.preguntaRepository.findById(id).get();
+        if (pregunta == null || pregunta.getEliminada())
             throw new RuntimeException("La pregunta a eliminar no existe");
-        this.preguntaRepository.deleteById(id);
+        pregunta.setEliminada(true);
+        this.preguntaRepository.save(pregunta);
     }
 
     public Pregunta obtener(Integer id) {
         if (id == null)
             throw new RuntimeException("No se tiene un id con el que buscar la pregunta");
         Optional<Pregunta> resultado = this.preguntaRepository.findById(id);
-        if (!resultado.isPresent())
+        if (!resultado.isPresent() || resultado.get().getEliminada())
             throw new RuntimeException("La pregunta a buscar no existe");
         Pregunta pregunta = resultado.get();
         pregunta.setListaRespuestas(this.respuestaServices.obtenerRespuestas(pregunta));
