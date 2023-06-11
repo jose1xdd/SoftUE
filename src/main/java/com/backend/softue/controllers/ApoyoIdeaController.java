86abf6e36db3a9ac00d3f1c179094bd5dfafbf69
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-06-11 17:13:39 -0500

    test

--- src/main/java/com/backend/softue/controllers/ApoyoIdeaController.java
+++ src/main/java/com/backend/softue/controllers/ApoyoIdeaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/apoyo")
+@RequestMapping(value = "/apoyoIdea")
 public class ApoyoIdeaController {
     @GetMapping("/test")
     public ResponseEntity<?> test() {
