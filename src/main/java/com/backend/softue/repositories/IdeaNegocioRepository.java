7be6bfbacfda8113813747f5e0f9f90caf13f6b0
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-06-10 22:54:44 -0500

    fix

--- src/main/java/com/backend/softue/repositories/IdeaNegocioRepository.java
+++ src/main/java/com/backend/softue/repositories/IdeaNegocioRepository.java
@@ -19,13 +19,13 @@ public interface IdeaNegocioRepository extends JpaRepository<IdeaNegocio, Intege
 
     @Query(value = "SELECT DISTINCT i.* FROM idea_negocio i " +
             "LEFT JOIN area_conocimiento ac ON i.area_enfoque = ac.id " +
-            "LEFT JOIN idea_planteada ip ON i.id = ip.idea_negocio_id " +
-            "LEFT JOIN evaluacion_idea ei ON i.id = ei.idea_negocio " +
-            "WHERE ((:estudianteCodigo IS NULL) OR (i.codigo_estudiante_lider = :estudianteCodigo OR ip.estudiante_codigo = :estudianteCodigo)) " +
+            "LEFT JOIN plan_presentado pp ON i.id = pp.plan_negocio_id " +
+            "LEFT JOIN evaluacion_plan ep ON i.id = ep.plan_negocio " +
+            "WHERE ((:estudianteCodigo IS NULL) OR (i.codigo_estudiante_lider = :estudianteCodigo OR pp.estudiante_codigo = :estudianteCodigo)) " +
             "AND (:docenteCodigo IS NULL OR i.tutor_codigo = :docenteCodigo) " +
             "AND (:areaConocimientoNombre IS NULL OR ac.nombre = :areaConocimientoNombre) " +
             "AND (:estado IS NULL OR i.estado = :estado) " +
-            "AND ((:fechaInicio IS NULL AND :fechaFin IS NULL) OR (ei.fecha_corte BETWEEN :fechaInicio AND :fechaFin)) ",
+            "AND ((:fechaInicio IS NULL AND :fechaFin IS NULL) OR (ep.fecha_corte BETWEEN :fechaInicio AND :fechaFin)) ",
             nativeQuery = true)
     public List<IdeaNegocio> findByFilters(
             @Param("docenteCodigo") String docenteCodigo,
