196feece6444c842115bb740130c21cfb91f788a
Julian <josejulianalre@ufps.edu.co>
2023-06-10 19:26:48 -0500

    Creaccion de filtros de docente de apoyo y docente evaluador de plan de negocio

--- src/main/java/com/backend/softue/controllers/PlanNegocioController.java
+++ src/main/java/com/backend/softue/controllers/PlanNegocioController.java
@@ -127,4 +127,34 @@ public class PlanNegocioController {
             return ResponseEntity.badRequest().body(new ResponseError(e.getClass().toString(),e.getMessage(),e.getStackTrace()[0].toString()));
         }
     }
+
+    @CheckSession(permitedRol = {"estudiante", "coordinador", "administrativo", "docente"})
+    @PostMapping("/PlanesDocentesApoyo")
+    public ResponseEntity<?> listarPlanesDocenteApoyo(
+            @RequestParam(required = false) Integer docenteCodigo,
+            @RequestParam(required = false) Integer estudianteCodigo,
+            @RequestParam(required = false) Integer area,
+            @RequestParam(required = false) String estado){
+        try {
+            return ResponseEntity.ok(this.planNegocioServices.listarPlanesDocenteApoyo(docenteCodigo,estudianteCodigo,area,estado));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ResponseError(e.getClass().toString(), e.getMessage(), e.getStackTrace()[0].toString()));
+        }
+    }
+
+    @CheckSession(permitedRol = {"estudiante", "coordinador", "administrativo", "docente"})
+    @PostMapping("/PlanesDocentesEvaluadores")
+    public ResponseEntity<?> listarPlanesDocenteEvaluador(
+            @RequestParam(required = false) Integer docenteCodigo,
+            @RequestParam(required = false) Integer estudianteCodigo,
+            @RequestParam(required = false) Integer area,
+            @RequestParam(required = false) String estado,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaInicio,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaFin){
+        try {
+            return ResponseEntity.ok(this.planNegocioServices.listarPlanesDocenteEvaluador(docenteCodigo,estudianteCodigo,area,estado,fechaInicio,fechaFin));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ResponseError(e.getClass().toString(), e.getMessage(), e.getStackTrace()[0].toString()));
+        }
+    }
 }
--- src/main/java/com/backend/softue/repositories/PlanNegocioRepository.java
+++ src/main/java/com/backend/softue/repositories/PlanNegocioRepository.java
@@ -1,4 +1,5 @@
 package com.backend.softue.repositories;
+import com.backend.softue.models.IdeaNegocio;
 import com.backend.softue.models.PlanNegocio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface PlanNegocioRepository extends JpaRepository<PlanNegocio, Integer> {
@@ -35,4 +37,42 @@ public interface PlanNegocioRepository extends JpaRepository<PlanNegocio, Intege
             @Param("estado") String estado,
             @Param("fechaInicio") LocalDate fechaInicio,
             @Param("fechaFin") LocalDate fechaFin);
+
+    @Query(value = "SELECT DISTINCT plan.* FROM plan_negocio plan " +
+            "JOIN docente_apoyo_plan dap ON (idea.id = dap.plan_negocio_id)" +
+            "JOIN plan_presentado pp ON (idea.id = pp.plan_negocio_id)" +
+            "WHERE " +
+            "(dap.docente_codigo = :docente_codigo OR :docente_codigo IS NULL) AND" +
+            "(pp.estudiante_codigo = :estudiante_codigo OR idea.codigo_estudiante_lider = :estudiante_codigo OR  :estudiante_codigo IS NULL) AND" +
+            "(plan.area_enfoque = :area OR :area IS NULL) AND" +
+            "(plan.estado = :estado OR :estado IS NULL)"
+            , nativeQuery = true)
+    Set<PlanNegocio> findByDocenteApoyoFiltros(
+            @Param("docente_codigo") Integer id,
+            @Param("estudiante_codigo") Integer estudiante_codigo,
+            @Param("area") Integer area,
+            @Param("estado") String estado
+    );
+
+    @Query(value = "SELECT DISTINCT plan.* FROM plan_negocio plan " +
+            "JOIN evaluacion_plan ep ON (plan.id = ep.plan_negocio) " +
+            "JOIN calificacion_plan cp ON (ep.id = cp.evaluacion_plan_id) " +
+            "JOIN plan_presentado pp ON (plan.id = pp.plan_negocio_id)" +
+            "WHERE" +
+            "(cp.Docente_codigo = :docente_codigo OR :docente_codigo IS NULL) AND" +
+            "(pp.estudiante_codigo = :estudiante_codigo OR plan.codigo_estudiante_lider = :estudiante_codigo OR  :estudiante_codigo IS NULL) AND" +
+            "(plan.area_enfoque = :area OR :area IS NULL) AND" +
+            "(plan.estado = :estado OR :estado IS NULL) AND" +
+            "((:fecha_inicio IS NULL AND :fecha_fin IS NULL) OR (ep.fecha_corte >= :fecha_inicio AND ep.fecha_corte <= :fecha_fin)) AND " +
+            "(cp.estado = 'pendiente')"
+            , nativeQuery = true)
+    Set<PlanNegocio> findByEvaluadorFiltros(
+            @Param("docente_codigo") Integer id,
+            @Param("estudiante_codigo") Integer estudiante_codigo,
+            @Param("area") Integer area,
+            @Param("estado") String estado,
+            @Param("fecha_inicio") LocalDate fecha_inicio,
+            @Param("fecha_fin") LocalDate fecha_fin
+
+    );
 }
