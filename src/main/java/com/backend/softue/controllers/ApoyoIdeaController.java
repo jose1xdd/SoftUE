35a93fc6509de2b83b44067db8a0222e41158192
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-06-11 17:06:52 -0500

    fix

--- src/main/java/com/backend/softue/controllers/ApoyoIdeaController.java
+++ src/main/java/com/backend/softue/controllers/ApoyoIdeaController.java
@@ -1,12 +1,14 @@
 package com.backend.softue.controllers;
 
+import com.backend.softue.services.TestServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/apoyoIdea")
+@RequestMapping(value = "apoyo")
 public class ApoyoIdeaController {
     @GetMapping("/test")
     public ResponseEntity<?> test() {
