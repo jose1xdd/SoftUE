d7e0d885e70cdb713311d3c0d23da8014356bbb4
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-06-11 16:31:15 -0500

    fix

--- src/main/java/com/backend/softue/controllers/DocenteApoyoPlanController.java
+++ src/main/java/com/backend/softue/controllers/DocenteApoyoPlanController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/albin")
+@RequestMapping(value = "/albin")
 public class DocenteApoyoPlanController {
     @Autowired
     private DocenteApoyoPlanServices docenteApoyoPlanServices;
