f36ac9eccdbf7f96ad89360ef89748b87252d855
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-06-11 16:43:19 -0500

    fix

--- src/main/java/com/backend/softue/controllers/DocenteApoyoIdeaController.java
+++ src/main/java/com/backend/softue/controllers/DocenteApoyoIdeaController.java
@@ -11,36 +11,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/docenteApoyoIdea")
 public class DocenteApoyoIdeaController {
-
-    @Autowired
-    private DocenteApoyoIdeaServices docenteApoyoIdeaServices;
-
     @GetMapping("/test")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok("pingpong");
-    }
-
-    @CheckSession(permitedRol = {"docente"})
-    @PostMapping("/a")
-    public ResponseEntity<?> agregarDocenteApoyo(@RequestHeader("X-Softue-JWT") String jwt, @RequestParam String tituloIdea, @RequestParam String correoDocente) {
-        try {
-            this.docenteApoyoIdeaServices.agregarDocenteApoyo(jwt, tituloIdea, correoDocente);
-            return ResponseEntity.ok(new ResponseConfirmation("El docente de apoyo se agrego correctamete a la idea de negocio"));
-        }
-        catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ResponseError(e.getClass().toString(),e.getMessage(),e.getStackTrace()[0].toString()));
-        }
-    }
-
-    @CheckSession(permitedRol = {"docente"})
-    @DeleteMapping("/b")
-    public ResponseEntity<?> eliminarDocenteApoyo(@RequestHeader("X-Softue-JWT") String jwt, @RequestParam String tituloIdea, @RequestParam String correoDocente) {
-        try {
-            this.docenteApoyoIdeaServices.eliminarDocenteApoyo(jwt, tituloIdea, correoDocente);
-            return ResponseEntity.ok(new ResponseConfirmation("El docente de apoyo se elimino correctamete de la idea de negocio"));
-        }
-        catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ResponseError(e.getClass().toString(),e.getMessage(),e.getStackTrace()[0].toString()));
-        }
     }
 }
