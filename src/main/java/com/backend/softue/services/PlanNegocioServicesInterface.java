69ef35ddb0edb873e4d3bcbba40a43462f093b04
Jose David Castillo <josedavidcabu@ufps.edu.co>
2023-06-11 22:58:52 -0500

    fix

--- src/main/java/com/backend/softue/services/PlanNegocioServicesInterface.java
+++ src/main/java/com/backend/softue/services/PlanNegocioServicesInterface.java
@@ -36,6 +36,8 @@ public class PlanNegocioServicesInterface {
             throw new RuntimeException("No se puede modificar un plan de negocio aprobado");
         Docente docente = this.docenteServices.obtenerDocente(docenteEmail);
         if (docente == null) throw new RuntimeException("No se encontro un docente con ese email");
+        Set<DocenteApoyoPlan> docentes = planNegocio.getDocentesApoyo();
+        if(docentePertenece(docentes,docente)) throw new RuntimeException("El docente tutor no puede ser docente de apoyo");
         this.emailService.enviarEmailTutorPlan(docenteEmail, titulo, docente.getNombre() + " " + docente.getApellido(), docente.getArea());
     }
     public PlanNegocio confirmarTutor(PlanNegocio planNegocio) {
@@ -47,8 +49,6 @@ public class PlanNegocioServicesInterface {
             if(planNegocio == null ) throw  new RuntimeException("se mandó mal el titulo de la idea de negocio");
             Docente docente = this.docenteServices.obtenerDocente(this.encrypth.getJwt().getKey(jwt));
             planNegocio.setTutor(docente);
-            Set<DocenteApoyoPlan> docentes = planNegocio.getDocentesApoyo();
-            if(docentePertenece(docentes,docente)) throw new RuntimeException("El docente tutor no puede ser docente de apoyo");
             if(this.confirmarTutor(planNegocio) != null);return "Docente Asignado";
         }
         return "El docente rechazo";
